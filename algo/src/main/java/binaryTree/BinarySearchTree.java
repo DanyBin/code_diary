5a5e36b1344409e7f54bddf7aeb219300c29c870
shanbinbin <13466592524@163.com>
2019-10-22 20:04:32 +0800

    binary tree

--- algo/src/main/java/binaryTree/BinarySearchTree.java
+++ algo/src/main/java/binaryTree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package binaryTree;
 
+import org.junit.Test;
+import until.TreePrintUtil;
+
 /**
  * @ClassName BinarySearchTree
  * @Author bin
@@ -18,21 +21,119 @@ package binaryTree;
  *               如果「给定值」> 根节点的值，判断&遍历右子树，当右子节点为空时，插入数据
  *               如果「根节点为空」，则之间当称根节点
  *
+ *      删除操作-
+ *              1.「要删除节点」没有子节点，更新父节点指针为null即可
+ *              2.「要删除节点」只有一个节点（左子节点/右子节点），更新父节点指针 ——> 「要删除节点」的下一个节点
+ *              3.「要删除节点」有两个节点，
+ *                  a. 找到右子树中最小节点，把它替换成「要删除节点」
+ *                  b. 删除最小节点
+ *
  * @Link TODO
  **/
 public class BinarySearchTree {
 
-    TreeNode root;
+    BinaryTreeNode root;
+
+    @Test
+    public void test() {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        int[] array = {8, 4, 7, 2, 9, 5, 10};
+        for (int v : array) {
+            binarySearchTree.insert(v);
+        }
+        TreePrintUtil.pirnt(binarySearchTree.root);
+        binarySearchTree.delete(4);
+        TreePrintUtil.pirnt(binarySearchTree.root);
 
 
+    }
 
+
+    /**
+     * 删除操作
+     *
+     * @param data
+     */
+    public Boolean delete(int data) {
+        BinaryTreeNode p = this.root;
+        BinaryTreeNode pp = null;    //记录父节点
+        while (p != null && p.data != data) {
+            pp = p;
+            if (data > p.data) {
+                p = p.right;
+            } else {
+                p = p.left;
+            }
+        }
+
+        if (p == null) {
+            return false;
+        }
+
+
+        //删除两个节点
+        if (p.left != null && p.right != null) {
+            BinaryTreeNode minP = p.right;  //右子树
+            BinaryTreeNode minPP = p;
+            while (minP.left != null) {
+                minPP = minP;
+                minP = minP.left;
+            }
+
+            p.data = minP.data; //替换数据
+            p = minP;           //删除minP -- 删除最小节点。 采用下面的方法
+            pp = minPP;
+
+        }
+
+        //删除叶子节点或一个节点
+        BinaryTreeNode child; //p的子节点
+        if (p.left != null) child = p.left;
+        else if (p.right != null) child = p.right;
+        else child = null;
+
+        if (pp == null) root = child; //删除根节点
+        else if (pp.left == p) pp.left = child;
+        else pp.right = child;
+
+        return true;
+    }
+
+    /**
+     * 新增操作
+     *
+     * @param value
+     */
+    public void insert(int value) {
+        BinaryTreeNode newNode = new BinaryTreeNode(value);
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        BinaryTreeNode tree = this.root;
+        while (tree != null) {
+            if (value < tree.data) {
+                if (tree.left == null) {
+                    tree.left = newNode;
+                    return;
+                }
+                tree = tree.left;
+            } else {
+                if (tree.right == null) {
+                    tree.right = newNode;
+                    return;
+                }
+                tree = tree.right;
+            }
+        }
+    }
     /**
      * 查找操作
      * @param value
      * @return
      */
-    public TreeNode find(int value){
-        TreeNode tree = this.root;
+    public BinaryTreeNode find(int value) {
+        BinaryTreeNode tree = this.root;
         while (tree != null){
             if(value < tree.data){
                 tree = tree.left;
@@ -46,13 +147,34 @@ public class BinarySearchTree {
     }
 
 
-    public static class TreeNode{
+    private static class BinaryTreeNode implements TreePrintUtil.TreeNode {
         int data;
-        TreeNode left;
-        TreeNode right;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
 
-        public TreeNode(int data){
-             this.data = data;
+        public BinaryTreeNode(int data){
+            this.data = data;
+        }
+
+
+        @Override
+        public String toString() {
+            return "[" + data + "]";
+        }
+
+        @Override
+        public String getPrintInfo() {
+            return toString();
+        }
+
+        @Override
+        public TreePrintUtil.TreeNode getLeftChild() {
+            return left;
+        }
+
+        @Override
+        public TreePrintUtil.TreeNode getRightChild() {
+            return right;
         }
     }
 }
